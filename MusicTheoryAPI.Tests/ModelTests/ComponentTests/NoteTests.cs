a0cd6f8b032e515072df001f775f6ec6835dfe7a
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-12-20 19:57:46 +0000

    feat: Add Note class and tests

---
+++ MusicTheoryAPI.Tests/ModelTests/ComponentTests/NoteTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using MusicTheoryAPI.Model.Components;
+
+namespace MusicTheoryAPI.Tests.ModelTests.ComponentTests
+{
+    [TestFixture]
+    public class NoteTests
+    {
+        [TestCase(-1)]
+        [TestCase(12)]
+        public void PitchSetter_WhenInputInvalid_ThrowsArgumentOutOfRangeException(int pitchClass)
+        {
+            Action action = () => new Note() { Pitch = pitchClass };
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Test]
+        public void PitchSetter_WithValidInput_SetsProperty([Range(0,11)] int pitchClass)
+        {
+            var note = new Note() { Pitch = pitchClass };
+            note.Pitch.Should().Be(pitchClass);
+        }
+
+        [TestCase(-1)]
+        public void DurationSetter_WithNegativeInput_ThrowsArgumentOutOfRangeException(float duration)
+        {
+            Action action = () => new Note() { Duration = duration };
+            action.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Test]
+        public void DurationSetter_WithPositiveInput_SetsProperty([Range(0.125, 8, 0.125)] double duration)
+        {
+            var note = new Note() { Duration = (float) duration };
+            note.Duration.Should().Be((float) duration);
+        }
+    }
+}
---
+++ MusicTheoryAPI/Model/Component/Note.cs
@@ -0,0 +1,44 @@
+﻿namespace MusicTheoryAPI.Model.Components
+{
+    public class Note
+    {
+        /// <summary>
+        /// The pitch class set value of the note from 0 to 11.
+        /// </summary>
+        private int _pitch;
+        public int Pitch 
+        { 
+            get => _pitch;
+            set
+            {
+                if (value < 0 || value > 11)
+                {
+                    throw new System.ArgumentOutOfRangeException("Pitch must be > 0 and <= 11.");
+                }
+                _pitch = value;
+            }
+        }
+
+        /// <summary>
+        /// The duration of the note in beats.
+        /// </summary>
+        private float _duration = 1;
+        public float Duration 
+        { 
+            get => _duration;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new System.ArgumentOutOfRangeException("Duration must be > 0.");
+                }
+                _duration = value;
+            }
+        }
+
+        /// <summary>
+        /// The octave of the note, where middle C is C4.
+        /// </summary>
+        public int Octave { get; set; } = 4;
+    }
+}
